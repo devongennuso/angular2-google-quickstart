6c35650d38858d03790ec9aa81c63654cf1d8dfe
Devon Gennuso <devon.gennuso@gmail.com>
2016-06-30 21:39:26 -0500

    angular.io quickstart setup

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2.0 App</h1>'
+});
+
+export class AppComponet {}
+
+/*
+* Every Angular app has at least one root component,
+* conventionally named AppComponent, that hosts the client
+* user experience. Components are the basic building blocks
+* of Angular applications. A component controls a portion of
+* the screen — a view — through its associated template.
+*/
