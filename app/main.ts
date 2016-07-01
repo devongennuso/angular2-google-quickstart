9f50d6f9f2d4fe5619f97fff9fbbccda06816a4e
Devon Gennuso <devon.gennuso@gmail.com>
2016-06-30 23:18:08 -0500

    angular bootstrap

---
+++ app/main.ts
@@ -0,0 +1,10 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
+
+/*
+* Angular's browser `bootstrap` function
+* the application root component AppComponent
+* call `bootstrap` with `AppComponent`
+*/
